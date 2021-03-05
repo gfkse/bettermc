3ede63c4ba107e7a39416d1bcccb23bea8112768
Andreas Kersting <andreas.kersting@gfk.com>
2021-03-05 08:08:28 +0100

    test SEM_UNDO

--- tests/testthat/test-semv.R
+++ tests/testthat/test-semv.R
@@ -10,3 +10,12 @@ test_that("system v semaphores work", {
   expect_error(semv_unlink(sid))
   expect_error(semv_open(-1))
 })
+
+test_that("undo works for system v semaphores", {
+  sid <- semv_open(1)
+  job <- parallel::mcparallel({semv_wait(sid); semv_wait(sid); TRUE})
+  expect_null(parallel::mccollect(job, wait = FALSE, timeout = 1))  # i.e. blocking
+  parallel:::mckill(job, signal = tools::SIGKILL)
+  semv_wait(sid)  # will only return if first wait in child was undone after kill
+  semv_unlink(sid)
+})
